5195d8ea1a80603b96439b43df55695d184bfe7a
Niles O'Neil <nilesoneil@gmail.com>
2022-05-07 00:49:58 -0700

    create topping cost and size cost prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
 function Pizza (size,toppings) {
   this.size = size;
   this.toppings = toppings;
+  this.toppingPrice;
+  this.sizePrice;
+  this.price;
 }
 
+Pizza.prototype.calcToppingsCost = function() {
+  if (this.toppings === "pineapple") {
+    this.price += 1;
+  } else if (this.toppings === "jalapeno") {
+    this.price += 1.5;
+  } else if (this.toppings === "pepperoni") {
+    this.price += 2;
+  }
+}
+
+Pizza.prototype.calcSizeCost = function() {
+  if (this.size === "small") {
+    this.sizePrice += 12;
+  } else if (this.size === "medium") {
+    this.sizePrice += 15;
+  } else if (this.size === "large") {
+    this.sizePrice += 18;
+  }
+}
 
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
